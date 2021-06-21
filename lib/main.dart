252dd982bd30279a768716708e07b31bb1986fcb
Daksh kumar <dakshdhull@gmail.com>
2021-06-21 23:14:08 +0530

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,14 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeeper = [];
+  List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.'
+  ];
+  List<bool> answers = [false, true, true];
+  int questionNumber = 0;
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -39,7 +47,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[questionNumber],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -52,41 +60,64 @@ class _QuizPageState extends State<QuizPage> {
         Expanded(
           child: Padding(
             padding: EdgeInsets.all(15.0),
-            child: FlatButton(
-              textColor: Colors.white,
+            child: Container(
               color: Colors.green,
-              child: Text(
-                'True',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20.0,
+              child: TextButton(
+                child: Text(
+                  'True',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                  ),
                 ),
+                onPressed: () {
+                  bool correctAnswer = answers[questionNumber];
+                  if (correctAnswer == true) {
+                    print('user got it right');
+                  } else {
+                    print('user got it wrong');
+                  }
+                  setState(() {
+                    questionNumber++;
+                  });
+                  //The user picked true.
+                },
               ),
-              onPressed: () {
-                //The user picked true.
-              },
             ),
           ),
         ),
         Expanded(
           child: Padding(
             padding: EdgeInsets.all(15.0),
-            child: FlatButton(
+            child: Container(
               color: Colors.red,
-              child: Text(
-                'False',
-                style: TextStyle(
-                  fontSize: 20.0,
-                  color: Colors.white,
+              child: TextButton(
+                child: Text(
+                  'False',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                  ),
                 ),
+                onPressed: () {
+                  bool correctAnswer = answers[questionNumber];
+                  if (correctAnswer == false) {
+                    print('user got it right');
+                  } else {
+                    print('user got it wrong');
+                  }
+                  setState(() {
+                    questionNumber++;
+                  });
+                  //The user picked true.
+                },
               ),
-              onPressed: () {
-                //The user picked false.
-              },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(
+          children: scoreKeeper,
+        )
       ],
     );
   }
@@ -97,3 +128,33 @@ question1: 'You can lead a cow down stairs but not up stairs.', false,
 question2: 'Approximately one quarter of human bones are in the feet.', true,
 question3: 'A slug\'s blood is green.', true,
 */
+
+/*
+Icon(
+Icons.check,
+color: Colors.green,
+),
+Icon(
+Icons.close,
+color: Colors.red,
+),
+
+setState(() {
+scoreKeeper.add(
+Icon(
+Icons.check,
+color: Colors.green,
+),
+);
+});
+
+setState(() {
+scoreKeeper.add(
+Icon(
+Icons.close,
+color: Colors.red,
+),
+);
+});
+
+ */
